c9201675ce660c44f02df10e57d5a4e97599ba10
InnaGennadievna <mishaginaig@gmail.com>
2022-11-26 23:35:14 +0300

    Task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,23 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = 
+    {client: '', 
+    order: {},
+    goods: []}; 
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+     for (let i = 0; i < countOfGoods; i += 1) {
+       let object = {title: {}, count : {}};
+       object.title = goods[i].title;
+       object.count = goods[i].count;
+       data.goods.push(object);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    data.order.sum = sum;
+    data.client = name + ' ' + phone;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
+
 }
